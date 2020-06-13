593277da0a91c2c14893cf0c6ff315db2dd66d31
piemekanika@hotmail.com <piemekanika@hotmail.com>
2020-06-13 12:55:20 +0700

    todo: remove this code

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://xlora-back.piemekanika.dev/');
+// const socket = io('ws://localhost:3000');
 
 export default socket;
