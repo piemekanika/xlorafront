128b15ae9af4e228c876ff756640bd323fbb1fca
piemekanika@hotmail.com <piemekanika@hotmail.com>
2020-06-15 20:46:18 +0700

    little refactor

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
     <app-header></app-header>
-<!--    <app-chat></app-chat>-->
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue';
-// import AppChat from '@/components/AppChat.vue';
 export default {
   name: 'App',
   components: {
     AppHeader,
-    // AppChat,
-  }
-}
+  },
+};
 </script>
-
